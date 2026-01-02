70eb6fa6fbe3706ccd71a3deb20d9b308a7211f3
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2026-01-02 17:18:13 +0530

    initialize backend project with Express and MongoDB

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+import { connectDB } from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database", error);
+    process.exit(1);
+  });
