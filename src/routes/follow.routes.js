16e0265a4a78134f550e393229c50125fb05fd6c
HimanshuMahajan123 <147161044+HimanshuMahajan123@users.noreply.github.com>
2026-01-16 22:40:47 +0530

    rename likes endpoints

--- src/routes/follow.routes.js
+++ src/routes/follow.routes.js
@@ -3,15 +3,20 @@ import {
   addFollow,
   removeFollow,
   getFollowers,
-  getFollowing
+  getFollowing,
 } from "../controllers/follow.controller.js";
-import { verifyjwt } from "../middlewares/auth.middlewares.js"; 
+import { verifyjwt } from "../middlewares/auth.middlewares.js";
 import { validateObjectId } from "../validators/index.js";
 
 const router = Router();
 
 router.post("/addFollow/:userId", verifyjwt, validateObjectId, addFollow);
-router.delete("/follow/:userId", verifyjwt, validateObjectId, removeFollow);
+router.delete(
+  "/deleteFollow/:userId",
+  verifyjwt,
+  validateObjectId,
+  removeFollow,
+);
 router.get("/getFollowers/:userId", verifyjwt, validateObjectId, getFollowers);
 router.get("/getFollowing/:userId", verifyjwt, validateObjectId, getFollowing);
 
